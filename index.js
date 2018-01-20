69bc7da5edc08ee5e343b3e39cfc358f6c13b6b3
satuevsky <satuevsky@gmail.com>
2018-01-20 16:33:10 +0300

    RedisState added to index.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
  * Created by Islam on 26.12.2016.
  */
 
-let MemoryState = require('./src/memory-state');
+let MemoryState = require('./src/memory-state'),
+    RedisState = require('./src/redis-state');
 
 module.exports = {
-    MemoryState
+    MemoryState,
+    RedisState,
 };
